19a4812cb1e125dd3dd4efbac4a882cfa3f234dd
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2020-07-25 22:29:03 +0530

    using pipes and piping to response

--- readWriteStreams.js
+++ readWriteStreams.js
@@ -3,7 +3,10 @@ let fs = require('fs');
 let myReadStream = fs.createReadStream(__dirname + '/Intro2.js');
 let myWriteStream = fs.createWriteStream(__dirname + '/Intro3.js');
 
-myReadStream.on('data', chunk => {
-    console.log('new chunk received');
-    myWriteStream.write(chunk);
-});
+// myReadStream.on('data', chunk => {
+//     console.log('new chunk received');
+//     myWriteStream.write(chunk);
+// });
+
+// Rather than doing all this manually, we can use pipes
+myReadStream.pipe(myWriteStream);
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 let http = require('http');
+let fs = require('fs');
 
 let server = http.createServer();
 
 server.on('request', (req, res) => {
     console.log(`The request url is ${req.url}`);
     res.writeHead(200, { 'Content-Type': 'text/plain'});
-    res.end('Hey!!!');
+    let myReadStream = fs.createReadStream(__dirname + '/Intro2.js');
+    // res is also a writeable stream. We can read stream and write it to res
+    myReadStream.pipe(res);
 });
 
 server.listen(5000, '127.0.0.1');
