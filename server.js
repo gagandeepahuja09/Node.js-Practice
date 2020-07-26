c18ad6a2fb6ee70bccec3e48555e14b00d8c3875
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2020-07-26 10:56:52 +0530

    implementing basic routing w/o express

--- server.js
+++ server.js
@@ -6,18 +6,30 @@ let server = http.createServer();
 server.on('request', (req, res) => {
     console.log(`The request url is ${req.url}`);
     res.writeHead(200, { 'Content-Type': 'text/html'});
-    // let myReadStream = fs.createReadStream(__dirname + '/index.html');
-    // let myReadStream = fs.createReadStream(__dirname + '/Intro2.js');
-    // res is also a writeable stream. We can read stream and write it to res
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    let obj = {
-        name: 'Gagandeep',
-        place: 'Bareilly',
-        job: 'Software Engineer',
-        age: 21,
+    if(req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
     }
-    // We can't directly do like this => we need to convert this to either string or chunk
-    res.end(JSON.stringify(obj));
+    else if(req.url === '/intro') {
+        res.writeHead(200, { 'Content-Type': 'text/plain'});
+        fs.createReadStream(__dirname + '/Intro2.js').pipe(res);
+    }
+    else if(req.url === '/data') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        let obj = {
+            name: 'Gagandeep',
+            place: 'Bareilly',
+            job: 'Software Engineer',
+            age: 21,
+        }
+        // We can't directly do like this => we need to convert this to either string or chunk
+        res.end(JSON.stringify(obj));
+    }
+    else {
+        res.writeHead(404,{ 'Content-Type': 'text/html' });
+        let page = fs.readFileSync('404.html');
+        res.end(page);
+    }
 });
 
 server.listen(5000, '127.0.0.1');
